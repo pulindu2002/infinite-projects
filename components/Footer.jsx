7afc2a6591016a5712c277656ee55713ed7fb793
VenujaVethmin <you@example.com>
2023-11-01 15:01:16 +0530

    test

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import { DividerHorizontalIcon } from "@radix-ui/react-icons";
+
+export default function Footer(){
+    return(
+        <div>
+            <h1>Hello fotter</h1>
+        </div>
+    )
+}
